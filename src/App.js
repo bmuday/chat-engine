64d48998ec0499db7b87d39208bd4e9db33fc6a9
bmuday <bmuday@live.fr>
2021-08-02 14:02:55 +0200

    add files && git push

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./components/Login";
+import Chats from "./components/Chats";
 
 export default function App() {
-  return <>Hello World!</>;
+  return (
+    <>
+      <Router>
+        {/* <AuthProvider> */}
+        <Switch>
+          {/* <Route path="/chats" component={Chats}></Route> */}
+          <Route path="/" component={Login}></Route>
+        </Switch>
+        {/* </AuthProvider> */}
+      </Router>
+    </>
+  );
 }
+test;
