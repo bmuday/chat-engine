52c9ed1621d80b294a43a308a00f61ae7505d8d7
bmuday <bmuday@live.fr>
2021-08-14 21:54:01 +0200

    change login

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,11 @@ import React from "react";
 import { GoogleOutlined, FacebookOutlined } from "@ant-design/icons";
 
 const Login = () => {
-  return <div id="login-page"><div/></div>;
+  return (
+    <div id="login-page">
+      <div></div>
+    </div>
+  );
 };
 
 export default Login;
